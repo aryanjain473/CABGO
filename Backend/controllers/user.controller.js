a3618cdf1088c7b3e601dc4f4aaafff5891ffd53
aryanjain473 <aryan.gj@somaiya.edu>
2025-04-01 20:50:51 +0530

    route created to register user

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,21 @@
+const userModel = require('../models/user.model');
+const userService = require('../services/user.services');
+const { validationResult } = require('express-validator');
+
+module.exports.registerUser = async (req, res, next ) => {
+
+    const errors =  validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    const { fullname, email, password } = req.body;
+    const newUser = new userModel({ fullname, email });
+
+    newUser.password = await newUser.hashPassword(password);
+
+    const user = await userService.createUser(newUser); 
+    const token = user.generateAuthToken();
+
+    res.status(201).json({ user, token });
+}
---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { body } = require('express-validator') ;
+const usercontroller = require('../controllers/user.controller');
+
+
+
+router.post('/register', [
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('fullname.firstname').isLength({ min : 3}).withMessage('First name must be at least 3 character long'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+
+   
+],
+usercontroller.registerUser
+)
+
+module.exports = router;
